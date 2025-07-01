9c1f9aa7e9cb457c0cf1c20264421fb212978de7
NickLucche <nlucches@redhat.com>
2025-07-01 11:58:48 +0200

    aliases
    Signed-off-by: NickLucche <nlucches@redhat.com>

---
+++ aliases.zsh
@@ -0,0 +1,9 @@
+alias p='python3'
+alias gs='git status'
+alias build='BUILDX_EXPERIMENTAL=1 docker buildx debug --invoke /bin/sh --on=error build .'
+alias rgf='rg --files | rg'
+pv() {
+    python -c "import $1; print(getattr($1, '__version__', 'Version not found'))"
+}
+alias gfu='git fetch upstream'
+
